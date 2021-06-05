ba89cb6a1820092999cbffc887260a0383dcb250
BilalRehman08 <bilalrehman080808@gmail.com>
2021-06-05 05:07:27 -0700

    GET POST

---
+++ class 8/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return ''' 
+<form action="/info" method="GET">
+<input type="text" name="user" placeholder="Full Name"><br>
+<input type="email" name="email" placeholder="Email"><br>
+<input type="password" name="pass" placeholder="password"><br>
+<input type="submit" value="Send">
+</form>
+'''
+
+
+@app.route("/info", methods=['GET', 'POST'])
+def info():
+    if request.method == "GET":
+        return "<h1>GET Method</h1> Name " + request.args.get('user') + "<br>" \
+            " Your Email:"+request.args.get('email')
+    else:
+        return "<h1>POST Method</h1>Name " + request.form['user'] + "<br>" \
+            " Email:"+request.form['email']
+
+
+app.run(debug=True)
